4eb4ddd74e0602662d73f8b9e9d6cfe55c9335c6
Lory LETICEE <loryleticee@gmail.com>
2020-06-08 22:03:09 +0200

    infos adds

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import './Home.scss'
-//import logo from '../../../images/logo.svg'
+import logo from '../../images/loryleticee.png'
 
 import { Helmet } from 'react-helmet'
-
-//import Index from '../../services/index'
 
 const Home = () => {
   return (
     <>
       <Helmet>
-        <link rel='shortcut icon' type='image/x-icon' href='dee/favicon.ico' />
+        <link rel='shortcut icon' type='image/x-icon' href='./favicon.ico' />
         <meta charSet='utf-8' />
         <meta name='description' content='Lory LETICEE official website' />
         <meta property='og:site_name' content='Lory LETICEE' />
         <meta property='og:title' content='Lory LETICEE' />
-        <meta property='og:url' content='https://www.Loryleticee.com' />
+        <meta property='og:url' content='http://www.Loryleticee.com' />
         <meta property='og:type' content='website' />
+        <meta property='og:image' content={logo} />
         <meta property='og:description' content='The official website of Lory LETICEE.' />
         <meta property='og:image' content={'logo'} />
         <meta itemProp='name' content='Lory LETICEE' />
-        <meta itemProp='url' content='https://www.loryleticee.com' />
+        <meta itemProp='url' content='http://www.loryleticee.com' />
         <meta itemProp='description' content='The official website of Lory LETICEE' />
         <meta name='twitter:title' content='Lory LETICEE' />
         <meta name='twitter:url' content='https://twitter.com/Loryleticee' />
@@ -28,9 +27,14 @@ const Home = () => {
         <meta name='twitter:description' content='The official website of Lory LETICEE.' />
         <meta name='description' content='The official website of Lory LETICEE.' />
         <title>Lory LETICEE</title>
-        <link rel='canonical' href='https://www.loryleticee.com' />
+        <link rel='canonical' href='http://www.loryleticee.com' />
       </Helmet>
-      {/*<div className='cv'><a href='https://www.doyoubuzz.com/lory-leticee' alt='cv' dangerouslySetInnerHTML={{ __html: 'Curriculum Viate' }}></a> </div>*/}
+      <div className='container'>
+        <div>
+          <p>M. LORY LETICEE</p>
+          <p>Full-stack Developper Junior</p>
+        </div>
+      </div>
     </>
   )
 }
