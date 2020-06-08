f5559c9ea28e5d33c87109e66d3f7b53de942a69
Lory LETICEE <loryleticee@gmail.com>
2020-06-08 18:22:33 +0200

    add logo lory leticee

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,14 +23,14 @@ const Home = () => {
         <meta itemProp='url' content='https://www.loryleticee.com' />
         <meta itemProp='description' content='The official website of Lory LETICEE' />
         <meta name='twitter:title' content='Lory LETICEE' />
-        <meta name='twitter:url' content='https://twitter.com/Lory LETICEE_' />
+        <meta name='twitter:url' content='https://twitter.com/Loryleticee' />
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:description' content='The official website of Lory LETICEE.' />
         <meta name='description' content='The official website of Lory LETICEE.' />
-        <title>Lory LETICEE </title>
+        <title>Lory LETICEE</title>
         <link rel='canonical' href='https://www.loryleticee.com' />
       </Helmet>
-      
+      <div className='cv'><a href='https://www.doyoubuzz.com/lory-leticee' alt='cv' dangerouslySetInnerHTML={{ __html: 'Curriculum Viate' }}></a> </div>
     </>
   )
 }
