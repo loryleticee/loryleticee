0322a87a2cf1be58f07931be6a9b80beda6ce6ca
Lory LÉTICÉE <loryleticee@gmail.com>
2020-11-27 00:59:22 +0100

    ..

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -24,6 +24,24 @@ const useStyles = makeStyles(styles);
 
 export default function HeaderLinks(props) {
   const classes = useStyles();
+
+
+  const downloadCv = () => {
+    fetch("http://51.195.42.185:4000")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('Vousus :', result.items)
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        (error) => {
+          console.log('TEST :', result.items)
+        }
+      )
+  }
+  
   return (
     <List className={classes.list}>
       <ListItem className={classes.listItem}>
@@ -51,7 +69,7 @@ export default function HeaderLinks(props) {
       </ListItem>
       <ListItem className={classes.listItem}>
         <Button
-          href="http://51.195.42.185:4000"
+          href="#"
           color="transparent"
           target="_blank"
           className={classes.navLink}
