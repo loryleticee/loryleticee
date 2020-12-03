4cf8ea8e37114c43efd915088ffe153b2bbf11c4
Lory LÉTICÉE <loryleticee@gmail.com>
2020-12-03 03:43:21 +0100

    ..

--- server.js
+++ server.js
@@ -46,10 +46,11 @@ app.get('/fdj', function (req, res, next) {
   });
    
   var data = 
-  lineReader.on('line', function (line) {
-    arr = [...arr, line]
+  lineReader.on('line', function (line, last) {
+    console.log(last)
+    //data = [...data, line]
   });
-  res.send(arr)
+  res.send(data)
 	///res.sendFile(path.join(__dirname, "../../../../home/ubuntu/gain/logkeno/stats-"+(day.length < 2 ? '0'+day :day) +'-'+(month.length < 2 ? '0'+month :month)+'-'+year+".txt"))
 })
 
