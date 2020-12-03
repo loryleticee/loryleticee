da1d73fd9e240040c06efca4f590de4b66a6bb60
Lory LÉTICÉE <loryleticee@gmail.com>
2020-12-03 22:59:00 +0100

    ..

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/fdj', function (req, res, next) {
         lines.map((line) => {
           var splited = line.split(',')
           splited.length > 2 ? tab += ['_-------_']  :tab += [  splited[0] = splited[1] ];
-          fs.writeFile("./u.json", tab, (err) => {
+          fs.writeFile("./u.json", JSON.stringify(tab), (err) => {
             if (err) res.json(err);
           });
         })
