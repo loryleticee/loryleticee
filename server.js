05ca0cb11b896e3a6f6affc6264626f92e6984de
Lory LÉTICÉE <loryleticee@gmail.com>
2020-12-05 21:11:37 +0100

    ..

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const hostname = '0.0.0.0';
 //const hostname = '127.0.0.1';
 const port = 4000;
 
-console.log('TEST :', process.env.test)
+console.log('TEST :', process.env.TEST)
 
 
 var app = express()
